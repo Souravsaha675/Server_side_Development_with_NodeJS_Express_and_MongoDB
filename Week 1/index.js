8840a08d6b0c78e9d454854646fcc705621ed20e
sourav saha <souravsaha675@gmail.com>
2020-08-13 21:11:22 +0530

    index .js

---
+++ Week 1/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+
+const http = require("http");
+
+const morgan = require("morgan");
+
+const bodyParser = require("body-parser");
+
+const dishRouter = require('./routes/dishRouter');
+
+const promoRouter = require('./routes/promoRouter');
+
+const leaderRouter = require('./routes/leaderRouter');
+
+const hostname = "localhost";
+
+const port = 3000;
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + "/public"));
+
+app.use('/dishes', dishRouter);
+
+app.use('/promotions', promoRouter);
+
+app.use('/leaders', leaderRouter);
+
+app.use((req, res, next) => {
+    res.statusCode = 200;
+
+    res.setHeader("Content-Type", "text/html");
+
+    res.end("<html><body><h1>This is Express Server</h1></body></html>");
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`server listening at http://${hostname}:${port}`);
+});
