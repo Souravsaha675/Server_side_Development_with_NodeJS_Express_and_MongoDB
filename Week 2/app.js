fd8e5aa35398d824a9056c143644f264b31f2e33
sourav saha <souravsaha675@gmail.com>
2020-08-17 23:32:34 +0530

    Basic Authentication

--- Week 2/app.js
+++ Week 2/app.js
@@ -24,13 +24,13 @@ const Dishes = require("./models/dishes");
 
 const Promotions = require("./models/promotions");
 
-const Leaders = require("./models/leaders")
+const Leaders = require("./models/leaders");
 
 const url = "mongodb://localhost:27017/conFusion";
 
 const connect = mongoose.connect(url, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 connect.then(
@@ -56,6 +56,34 @@ app.use(
   })
 );
 app.use(cookieParser());
+
+function auth(req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+    var err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    next(err);
+    return;
+  }
+
+  var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+  var user = auth[0];
+  var pass = auth[1];
+  if (user == "admin" && pass == "password") {
+    next(); // authorized
+  } else {
+    var err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    next(err);
+  }
+}
+
+app.use(auth);
 
 app.use(express.static(path.join(__dirname, "public")));
 
