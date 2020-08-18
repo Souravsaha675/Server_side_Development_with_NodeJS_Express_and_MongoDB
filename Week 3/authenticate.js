213e5cf7e024293c18c7d775c962939b3017aaaf
sourav saha <souravsaha675@gmail.com>
2020-08-19 03:12:04 +0530

    assignment 3 task 1

--- Week 3/authenticate.js
+++ Week 3/authenticate.js
@@ -7,7 +7,7 @@ var jwt = require("jsonwebtoken"); // used to create, sign, and verify tokens
 
 var config = require("./config");
 
-passport.use(new LocalStrategy(User.authenticate()));
+exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -35,3 +35,14 @@ exports.jwtPassport = passport.use(
 );
 
 exports.verifyUser = passport.authenticate("jwt", { session: false });
+
+exports.verifyAdmin = function (req, res, next) {
+  if (req.user.admin) {
+    next();
+    return;
+  } else {
+    var err = new Error("You are not authorized to perform this operation!");
+    err.status = 403;
+    return next(err);
+  }
+};
