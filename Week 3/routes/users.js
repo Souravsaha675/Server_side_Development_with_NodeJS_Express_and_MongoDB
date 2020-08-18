a1b6b32217a152de065f31fbc50869f228f161d4
sourav saha <souravsaha675@gmail.com>
2020-08-18 23:11:05 +0530

    PassportJWT

--- Week 3/routes/users.js
+++ Week 3/routes/users.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 
 var User = require("../models/user");
 
+var authenticate = require("../authenticate");
+
 var passport = require("passport");
 
 var router = express.Router();
@@ -105,11 +107,24 @@ router.post("/signup", (req, res, next) => {
     }
   );
 });
-
-router.post("/login", passport.authenticate("local"), (req, res) => {
+/////////////////////////USING PASSPORT SESSION///////////////////
+/*router.post("/login", passport.authenticate("local"), (req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
   res.json({ success: true, status: "You are successfully logged in!" });
+});*/
+
+////////////////////////JWT TOKEN //////////////////////////////////
+
+router.post("/login", passport.authenticate("local"), (req, res) => {
+  var token = authenticate.getToken({ _id: req.user._id });
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.json({
+    success: true,
+    token: token,
+    status: "You are successfully logged in!",
+  });
 });
 
 router.get("/logout", (req, res) => {
